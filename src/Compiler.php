8739a2e9be891bc7af85f6bc798bcd1b4377a80a
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-04-01 22:16:29 +0200

    initial

---
+++ src/Compiler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tualo\Office\TextTemplates;
+
+use Tualo\Office\Basic\TualoApplication;
+use Tualo\Office\ExtJSCompiler\ICompiler;
+use Tualo\Office\ExtJSCompiler\CompilerHelper;
+
+class Compiler implements ICompiler
+{
+    public static function getFiles()
+    {
+        return CompilerHelper::getFiles(__DIR__, 'texttemplates', 10003);
+    }
+}
---
+++ src/sql/install/ds_class.sql
@@ -0,0 +1,7 @@
+delimiter ;
+
+
+insert
+    ignore into ds_class (class_name)
+values
+    ('Todo´s');
